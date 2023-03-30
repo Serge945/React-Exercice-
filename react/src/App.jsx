1012181c712ced572b65962f593f099b9dd28731
serge patrick acka <sergeacka@gmail.com>
2023-03-30 17:21:15 +0200

    new commit

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import MyTitle from "./components/MyTitle"
+import PokemondCard from "./components/pokemonCard"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,7 @@ function App() {
   return (
     <div>
       <MyTitle/>
+      <PokemondCard/>
     </div>
   )
 }
