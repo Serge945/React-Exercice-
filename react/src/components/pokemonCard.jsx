62984ca7c217cc45999bfe526ce9faa8ff6e59d1
serge patrick acka <sergeacka@gmail.com>
2023-03-31 05:46:50 +0200

    props

--- react/src/components/pokemonCard.jsx
+++ react/src/components/pokemonCard.jsx
@@ -1,10 +1,49 @@
-function PokemondCard(){
+// const pokemonList = [
+//     {
+//       name: "bulbasaur",
+//       imgSrc:
+//         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+//     },
+//     {
+//       name: "mew",
+//     },
+//   ];
+
+
+
+function PokemondCard(props){
+    console.log(props)
+    const pokemon = props.pokemon;
     return (
-        <figure>
-    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-         alt="Pokemon"/>
-    <figcaption>Bulbasaur</figcaption>
-</figure>
-    )
-}
+    <div>
+       <h2>{pokemon.name}</h2>
+       {pokemon.imgSrc ? ( 
+        <img src={pokemon.imgSrc} alt={pokemon.name} />
+        ) : (
+          <p>???</p>
+       )} 
+    </div>
+       );
+    }
+// function PokemondCard(){
+//     const pokemon = pokemonList[1]
+//     return (
+//     <div>
+//        <h2>{pokemon.name}</h2>
+//        {pokemon.imgSrc ? ( 
+//         <img src={pokemon.imgSrc} alt={pokemon.name} />
+//         ) : (
+//           <p>???</p>
+//        )} 
+//     </div>
+//        );
+//     }
+
+
+//         <figure>
+//     <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+//          alt="Pokemon"/>
+//     <figcaption>Bulbasaur</figcaption>
+// </figure>
+    
 export default PokemondCard;
